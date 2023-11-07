5b7864e38424089d0cad8b43874c4acaf2b0a9f5
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-11-06 21:23:03 -0500

    Welcome screen

--- src/Containers/Builder.tsx
+++ src/Containers/Builder.tsx
@@ -4,9 +4,12 @@ import { useEffect, useState } from "react";
 import linkstrLogo from "../Images/logo.svg";
 import linkIcon from "../Images/link.svg";
 import cancelIcon from "../Images/cancel.svg";
+import background from "../Images/background.png";
 
 const Builder = () => {
   const tempLinks = ["google.com", "youtube.com"];
+
+  const [showWelcome, setShowWelcome] = useState(true);
 
   const [links, setLinks] = useState([]);
 
@@ -33,7 +36,35 @@ const Builder = () => {
   const WelcomeOverlay = () => {
     return (
       <div>
-        <Header mode="welcome" />
+        <div className="absolute left-0 top-0 z-20 w-full h-[38rem] bg-white">
+          <div className="relative w-[60rem] mx-auto">
+            <img
+              src={background}
+              alt=""
+              className="absolute -right-6 -top-[82px] w-[36rem]"
+            />
+            <Header mode="welcome" />
+            <div className="flex flex-col">
+              <div className="h1 w-[44rem] mt-20">
+                Create your perfect link collection and share it with the world.
+              </div>
+              <button
+                className="button h-14 w-40 text-lg rounded-2xl mt-16"
+                onClick={() => setShowWelcome(false)}
+              >
+                Get Started
+              </button>
+              <div className="text-xs font-medium mt-6 pl-1">
+                <span className="text-black/40">No Login Required!</span>
+                <span> Learn more</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute left-0 top-0 z-10 bg-black/40 w-full min-h-full"
+          onClick={() => setShowWelcome(false)}
+        ></div>
       </div>
     );
   };
@@ -48,11 +79,7 @@ const Builder = () => {
       </div>
     );
 
-    const PublishButton = (
-      <div>
-        <button className="button">Publish</button>
-      </div>
-    );
+    const PublishButton = <button className="button h-8 px-3">Publish</button>;
 
     return (
       <div className="flex justify-between mt-8">
@@ -64,9 +91,7 @@ const Builder = () => {
   const TitleAndDescription = () => {
     return (
       <div className="text-center mx-auto mt-14">
-        <div className="font-serif text-6xl leading-none">
-          Top 10 travel destinations in India
-        </div>
+        <div className="h1">Top 10 travel destinations in India</div>
         <div className="text-black/40 text-xs">
           <span>curated by – </span>
           <span className="font-bold">SPARSH PALIWAL</span>
@@ -90,7 +115,7 @@ const Builder = () => {
   }) => {
     return (
       <div className="flex flex-col py-6 border-b relative">
-        <div className="flex justify-center items-center h-8 w-8 rounded-full opacity-60 cursor-pointer hover:bg-black/5 hover:opacity-90 active:bg-black/20 active:opacity-100 absolute -right-2.5 transition-all duration-200">
+        <div className="action-button -right-2.5">
           <img src={cancelIcon} alt="" />
         </div>
         <div className="flex flex-row items-center">
@@ -148,14 +173,15 @@ const Builder = () => {
 
   return (
     <div className="w-[60rem] mx-auto">
-      <WelcomeOverlay />
-      {/* <div className="w-full h-full blur-xl"></div> */}
-      <div className="w-full h-full">
-        <Header mode="edit" />
-        <div className="w-[40rem] mx-auto">
-          <TitleAndDescription />
-          <Collection />
-          <Footer />
+      {showWelcome && <WelcomeOverlay />}
+      <div className={`w-full h-full ${showWelcome ? "blur-xl" : ""}`}>
+        <div className="w-full h-full">
+          <Header mode="edit" />
+          <div className="w-[40rem] mx-auto">
+            <TitleAndDescription />
+            <Collection />
+            <Footer />
+          </div>
         </div>
       </div>
     </div>
