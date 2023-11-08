693ec7c85dca2e44a2332f61aa5779bf2a4301c2
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-11-08 18:02:43 -0500

    useEditable to update text inplace

--- src/Components/TitleAndDescription.tsx
+++ src/Components/TitleAndDescription.tsx
@@ -1,15 +1,58 @@
+import { useRef, useState } from "react";
+import { useEditable } from "use-editable";
+import { motion } from "framer-motion";
+
 const TitleAndDescription = () => {
+  const [title, setTitle] = useState("Top 10 travel destinations in India");
+  const [description, setDescription] = useState(
+    "Embark on a Journey Through India's Most Enchanting Destinations",
+  );
+  const [author, setAuthor] = useState("Sparsh Paliwal");
+
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const authorRef = useRef(null);
+
+  useEditable(titleRef, setTitle);
+  useEditable(descriptionRef, setDescription);
+  useEditable(authorRef, setAuthor);
+
+  const formattedDate = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <div className="mx-auto mt-14 text-center">
-      <div className="h1">Top 10 travel destinations in India</div>
-      <div className="text-xs text-black/40">
+      <motion.div
+        whileHover={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        whileTap={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+        className="h1 rounded-2xl pt-4 outline-none"
+        ref={titleRef}
+      >
+        {title}
+      </motion.div>
+      <div className="flex flex-row items-center justify-center text-xs text-black/40">
         <span>curated by – </span>
-        <span className="font-bold">SPARSH PALIWAL</span>
-        <span className="font-bold"> • 5 Nov 2022</span>
+        <motion.span
+          whileHover={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+          whileTap={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+          className="rounded-lg p-2 font-bold outline-none"
+          ref={authorRef}
+        >
+          {author}
+        </motion.span>
+        <span className="font-bold"> • {formattedDate} </span>
       </div>
-      <div className="pt-6 text-lg text-black/60">
-        Embark on a Journey Through India's Most Enchanting Destinations
-      </div>
+      <motion.div
+        whileHover={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        whileTap={{ backgroundColor: "rgba(0, 0, 0, 0.1)" }}
+        className="mt-4 rounded-lg p-2 text-lg text-black/60 outline-none"
+        ref={descriptionRef}
+      >
+        {description}
+      </motion.div>
     </div>
   );
 };
