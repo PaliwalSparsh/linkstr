da5f9b096e4569e92666a30e04096bd9459dc3d8
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-11-17 00:53:23 -0500

    Add loader

--- src/Containers/CollectionViewer.tsx
+++ src/Containers/CollectionViewer.tsx
@@ -6,12 +6,14 @@ import { defaultNodes, defaultMetadata } from "./CollectionBuilder";
 import AllNodes from "../Components/AllNodes";
 import { getLinkCollection } from "../nostr";
 import { Logo } from "../Components/Header";
+import Loader from "./Loader";
 
 interface CollectionViewerProps {
   npub: string;
 }
 
 const CollectionViewer = ({ npub }: CollectionViewerProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [collection, setCollection] = useState<Collection | null>(null);
 
   useEffect(() => {
@@ -19,30 +21,34 @@ const CollectionViewer = ({ npub }: CollectionViewerProps) => {
       let event = await getLinkCollection(npub);
       let parsedCollection = JSON.parse(event[0]?.content);
       setCollection(parsedCollection);
+      setIsLoading(false);
     }
     get();
   }, [npub]);
 
   return (
-    <div className="h-full w-full">
-      <div className="relative h-16">
-        <img
-          src={downArrowArt}
-          alt=""
-          className="absolute -top-12 left-1/2 h-36 -translate-x-1/2"
-        />
-      </div>
-      <div className="mx-auto w-full md:w-[40rem]">
-        <TitleAndDescription
-          mode="view"
-          metadata={collection?.metadata ?? defaultMetadata}
-        />
-        <AllNodes mode="view" nodes={collection?.nodes ?? defaultNodes} />
-        <div className="flex flex-row items-center justify-center pb-12 pt-32">
-          <Logo />
+    <>
+      {isLoading && <Loader />}
+      <div className="h-full w-full">
+        <div className="relative h-16">
+          <img
+            src={downArrowArt}
+            alt=""
+            className="absolute -top-12 left-1/2 h-36 -translate-x-1/2"
+          />
+        </div>
+        <div className="mx-auto w-full md:w-[40rem]">
+          <TitleAndDescription
+            mode="view"
+            metadata={collection?.metadata ?? defaultMetadata}
+          />
+          <AllNodes mode="view" nodes={collection?.nodes ?? defaultNodes} />
+          <div className="flex flex-row items-center justify-center pb-12 pt-32">
+            <Logo />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
